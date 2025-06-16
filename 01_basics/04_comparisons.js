1e86629f382807374d869011f9e589f1f72a116c
Anjani Kumar Ray <95203252+calm-ray@users.noreply.github.com>
2025-06-16 17:11:16 +0000

    comparisons in js

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,11 @@
+// console.log("2" < 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+
+// === is strict check, checks values as well as datatype
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false
