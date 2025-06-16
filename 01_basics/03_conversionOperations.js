c7ff1dd71942f5c889c4a5e01b11a3d7608831ea
Anjani Kumar Ray <95203252+calm-ray@users.noreply.github.com>
2025-06-16 16:57:58 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -46,3 +46,22 @@ let num = 33
 let stringNum = String(num)
 console.log(typeof stringNum);
 console.log(stringNum);
+
+// *************************Operations*************************
+
+num = 2
+console.log(num + num);
+console.log(num - num);
+console.log(num * num);
+console.log(num / num);
+console.log(num ** 3);
+console.log(num % 3);
+
+console.log("2" + 1) // 21
+console.log("1" + 2) // 12
+console.log("2" + 1 + 2) // 212
+console.log(2 + 1 + "3") // 33
+
+console.log(+true); // 1
+console.log(+""); // 0
+
