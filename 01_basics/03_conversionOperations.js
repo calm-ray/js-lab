10501075a71af71003060aae0fe39384639c30f6
Anjani Kumar Ray <95203252+calm-ray@users.noreply.github.com>
2025-06-16 16:38:37 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,48 @@
+let score = 33
+
+console.log(typeof score) // number
+
+score = "33"
+let varInNum = Number(score)
+console.log(typeof varInNum) // number
+console.log(varInNum) // 33
+
+score = "34avc"
+varInNum = Number(score)
+console.log(typeof varInNum) // number
+console.log(varInNum) // NaN
+
+score = null
+varInNum = Number(score)
+console.log(typeof varInNum) // number
+console.log(varInNum) // 0
+
+score = undefined
+varInNum = Number(score)
+console.log(typeof varInNum) // number
+console.log(varInNum) // NaN
+
+score = true
+varInNum = Number(score)
+console.log(typeof varInNum) // number
+console.log(varInNum) // 1 
+
+let isLoggedIn = 1
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof boolIsLoggedIn) // boolean
+console.log(boolIsLoggedIn) // true 
+
+isLoggedIn = ""
+boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof boolIsLoggedIn) // boolean
+console.log(boolIsLoggedIn) // false 
+
+isLoggedIn = "akr"
+boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof boolIsLoggedIn) // boolean
+console.log(boolIsLoggedIn) // true
+
+let num = 33
+let stringNum = String(num)
+console.log(typeof stringNum);
+console.log(stringNum);
