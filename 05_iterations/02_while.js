261038baab7efe2c01a85ed9316bed8881cf2d57
Anjani Kumar Ray <95203252+calm-ray@users.noreply.github.com>
2025-06-28 07:24:50 +0000

    while loop in js

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,18 @@
+// let i = 1
+// while (i <= 10) {
+//     console.log(`Value ${i}`);
+//     i+=2;
+// }
+
+// let myArry = ["flash", "batman", "superman"]
+// let arrIndex = 0;
+// while(arrIndex < myArry.length) {
+//     console.log(`Value is ${myArry[arrIndex]}`);
+//     arrIndex++;
+// }
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
